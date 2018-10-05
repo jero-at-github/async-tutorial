7b08a473a8d37c4ca565229445d8d7a434bd9e55
Lopez Navarro, Jeronimo <jeronimo.lopez-navarro@here.com>
2018-10-05 15:32:43 +0200

    Updated fat arrows

--- fat-arrows.js
+++ fat-arrows.js
@@ -1,39 +1,43 @@
     
-// example of classic function    
+/**
+ * Example of classic function decalaration
+ */
 var a = function(myParam) {    
     
     let myVar = myParam;        
     console.log(myVar);
 }    
 
-// example of fat arrow function
+/**
+ * Example of fat arrow function declaration
+ */
 var b = (myParam) => {
 
     let myVar = myParam;        
     console.log(myVar);
 }    
 
-// example of classic function which just returns a value
+/**
+ * Example of classic function declaration which just returns a value
+ */
 var c = function(myParam) {
     return myParam;
 }
 
-// example of fat arrow function which just returns a value
+/**
+ * Example of fat arrow function declaration which just returns a value 
+ */
 var d = (myParam) => {
     return myParam;
 }    
-var e = (myParam) => myParam;
-var f = myParam => myParam;
+
+var d2 = (myParam) => myParam;
+
+var d3 = myParam => myParam;
 
 
 /* ========= 
     Execution 
     ========= */
 
-a("hello!");
-b("hello!");
-
-console.log( c("hello!") );
-console.log( d("hello!") );
-console.log( e("hello!") );
-console.log( f("hello!") );
+console.log( d3("hello") );
