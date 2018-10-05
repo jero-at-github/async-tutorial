3c17a386fc91326e6f05329f6b7c309ea0df2bb1
Lopez Navarro, Jeronimo <jeronimo.lopez-navarro@here.com>
2018-10-05 15:55:55 +0200

    Updated callabacks

--- callbacks.js
+++ callbacks.js
@@ -1,6 +1,12 @@
 
+/**
+ * Function which returns a random number between 1 and 10
+ */
 let generateRnd = () => Math.floor(Math.random() * 10) + 1;
 
+/**
+ * Function which retreives the result of an asyc function NOT using callbacks
+ */
 var a = function() {
 
     setTimeout(function() {
@@ -9,7 +15,10 @@ var a = function() {
     }, 1000);
 
 }    
-    
+
+/**
+ * Function which retreives the result of an asyc function using callbacks
+ */
 let b = (myCallback) => {
 
     // generate random number after 1 second
@@ -24,7 +33,10 @@ let b = (myCallback) => {
 
 }
 
-let c = function(myCallback, previousRndNumber) {
+/**
+ * Dependent function example
+ */
+let c = function(myCallback, previousRndNumber = 0) {
 
     // generate random number after 1 second
     setTimeout(function() {
@@ -37,7 +49,53 @@ let c = function(myCallback, previousRndNumber) {
     }, 1000);
 }
 
-// pyramid of doom!
+
+/* ========= 
+    Execution 
+    ========= */
+
+/**
+ * Testing generateRnd function
+ */
+//console.log( generateRnd() );
+
+
+/**
+ * Trying to get the result of an async function
+ */
+//console.log( a() );
+
+
+/**
+ * Getting the result of an async function using callbacks
+ */
+
+/*
+//using fat arrows declaration
+b( (result) => {console.log(result)} );
+
+// using function declaration
+b(function(result) {
+    console.log(result)
+});
+*/
+
+
+/**
+ * Building a dependent function example
+ */
+/*
+c( (result) => {
+
+    console.log("Total sum: " + result);        
+});
+*/   
+ 
+
+/**
+ * Executing 5 dependent functions
+ */
+/*
 c(function(previousRndNumber) {
 
     c(function(previousRndNumber) {
@@ -59,8 +117,9 @@ c(function(previousRndNumber) {
     }, previousRndNumber);
 
 }, 0);
-    
+*/    
 
+/*
 let results = [];
 
 for (var conta = 0; conta <= 4; conta ++) {
@@ -73,19 +132,4 @@ for (var conta = 0; conta <= 4; conta ++) {
         }
     }, 0);
 }
-
-
-/* ========= 
-    Execution 
-    ========= */
-
-//let randomNumber = a();
-//console.log(randomNumber);        
-
-//b( (result) => {console.log(result)} );
-/*
-c( (result) => {
-
-    console.log("Total sum: " + result);        
-});   
-*/ 
+*/
